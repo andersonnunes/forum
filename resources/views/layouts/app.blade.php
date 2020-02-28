fbd7b84fd599cfbc94dd3aa1b6e826dd5f3f1a79
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-02-28 16:23:34 -0300

    A User Can Filter Threads By Popularity

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,14 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <style>
+        body { padding-bottom: 100px; }
+        .level { display: flex; align-items: center; }
+        .flex { flex: 1; }
+    </style>
 </head>
-<body style="padding-bottom: 100px;">
+<body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
@@ -46,6 +52,10 @@
                                         </a>
                                     </li>
                                 @endif
+
+                                <li>
+                                    <a class="dropdown-item" href="/threads?popular=1">Popular Threads</a>
+                                </li>
                             </ul>
                         </li>
                         <liv class="nav-item"><a class="nav-link" href ="/threads/create">New Thread</a></liv>
