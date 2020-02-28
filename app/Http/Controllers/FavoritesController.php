caf5a232b93c1963062d7dda867eb5635872afcf
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-02-28 16:45:21 -0300

    A User Can Favorite Replies

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Favorite;
+use App\Reply;
+use Illuminate\Http\Request;
+
+class FavoritesController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Store a new favorite in the database.
+     *
+     * @param  Reply $reply
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function store(Reply $reply)
+    {
+        return $reply->favorite();
+    }
+}
