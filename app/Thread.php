3262dab5d6aa70d95b901819f1d7e1da921e7176
Anderson Nunes <anderson.nunes1988@gmail.com>
2020-03-08 14:54:08 -0300

    Mark the Best Reply

--- app/Thread.php
+++ app/Thread.php
@@ -201,4 +201,14 @@ class Thread extends Model
 
         $this->attributes['slug'] = $slug;
     }
+
+    /**
+     * Mark the given reply as the best answer.
+     *
+     * @param Reply $reply
+     */
+    public function markBestReply(Reply $reply)
+    {
+        $this->update(['best_reply_id' => $reply->id]);
+    }
 }
